ee8ad7f3a4107762724a566eb61ef8cec2c4a388
shwntypar <shawnrada4@gmail.com>
2026-04-24 14:43:26 +0800

    installation of express and implementation of api

---
+++ Backend/src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '../../generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
